59ead6344c88cbe4d912ec382d2224bf948d7c26
Vangala Pranitej <vangalapranitej@gmail.com>
2026-04-08 11:39:51 +0530

    feat: register companies and superAdmin routes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// server/server.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -6,7 +7,9 @@ import seeding from "./database/seeding.js";
 import invoicesRouter from "./routes/invoices.js";
 import authRouter from "./routes/auth.js";
 import pdfRoutes from "./routes/pdf.js";
-import companyConfig from "./config.js";
+import companiesRouter from "./routes/companies.js";
+import superAdminRouter from "./routes/superAdmin.js";
+import config from "./config.js";
 
 dotenv.config();
 
@@ -19,15 +22,16 @@ app.use("/public", express.static("public"));
 const PORT = process.env.PORT || 5000;
 
 await connectDB();
-
 await seeding();
 
 app.use("/api/invoices", invoicesRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/pdf", pdfRoutes);
+app.use("/api/companies", companiesRouter);
+app.use("/api/super-admin", superAdminRouter);
 
 app.get("/", (_, res) =>
-  res.send({ ok: true, message: `${companyConfig.name} API` })
+  res.send({ ok: true, message: `${config.platform.name} API` })
 );
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
